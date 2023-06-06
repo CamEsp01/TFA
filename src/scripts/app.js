4f6646d7d070ac75852abf619db6527f59a9140e
Camille ESPERT <camilleespert@MBP-de-Camille.lan>
2023-06-06 23:35:59 +0200

    modifications page crédit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -21,27 +21,40 @@ for (let navTrigger of navTriggers) {
 }
 
 
-if (TriggerButton != undefined && TriggerButton != null){
-    let section = document.querySelectorAll('.section');
-    const navTriggers = document.querySelectorAll(".menu__el");
-    function activeLink(li){
-      navTriggers.forEach((item) => item.classList.remove('menu__el--active'));
-      li.classList.add('menu__el--active');
-    };
-    navTriggers.forEach((item) =>
-    item.addEventListener('click', function(){
-      activeLink(this);
-    }));
-    window.onscroll = () => {
-      section.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-        if (top >= offset && top < offset + height){
-          const target = document.querySelector(`[href='#${id}']`).parentElement;
-          activeLink(target);
-        };
-      });
-    };
+if (menuBurger != undefined && menuBurger != null){
+  let section = document.querySelectorAll('.section');
+  const navTriggers = document.querySelectorAll(".menu__el");
+  function activeLink(li){
+    navTriggers.forEach((item) => item.classList.remove('menu__el--active'));
+    li.classList.add('menu__el--active');
   };
+  navTriggers.forEach((item) =>
+  item.addEventListener('click', function(){
+    activeLink(this);
+  }));
+  window.onscroll = () => {
+    section.forEach(sec => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+      if (top >= offset && top < offset + height){
+        const target = document.querySelector(`[href='#${id}']`).parentElement;
+        activeLink(target);
+      };
+    });
+  };
+};
+
+// AFFICHAGE LEGENDE
+const btn_legende = document.querySelector("#btn_legende");
+if(btn_legende != undefined && btn_legende != null){
+	btn_legende.addEventListener("click", (e) =>{
+		const legende = document.querySelector("#legende");
+		legende.classList.toggle("hidden");
+	});
+}
+
+  
+
+
